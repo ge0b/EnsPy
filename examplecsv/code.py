53cd4f18c629ff7ea94273aa06234f68723fed0f
GeorgiosBoumis <62819177+GeorgiosBoumis@users.noreply.github.com>
2020-03-30 02:26:05 +0300

    Update code.py

--- examplecsv/code.py
+++ examplecsv/code.py
@@ -1,4 +1,4 @@
-# read a 5-member ensemble forecast from a .csv file and create a proper tidy dataframe
+# read an ensemble forecast from a .csv file and create a proper tidy dataframe
 from EnsPy import enspytools
 
 myfile = enspytools("/Users/georgiosboumis/Desktop/5-memberEnsemble.csv")
