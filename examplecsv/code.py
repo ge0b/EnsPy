b6844c2da96f93f11f18a906a9e4bf7c211ed2f6
Georgios Boumis <62819177+ge0b@users.noreply.github.com>
2020-05-19 14:49:40 +0300

    Update code.py

--- examplecsv/code.py
+++ examplecsv/code.py
@@ -4,3 +4,6 @@ from EnsPy import enspytools
 myfile = enspytools("/Users/georgiosboumis/Desktop/5-memberEnsemble.csv")
 mydataframe = myfile.enspyframe()
 myfile.enspygraph(mydataframe)
+
+# this will calculate the probability of exceeding 310 on 1990-03-14
+myfile.enspyprob(mydataframe, '1990-03-14 00', 310.0)
